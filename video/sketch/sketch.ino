fe27a9ace744f9a72838a41f7ef25c5c41c99c53
llo <dayaftereh@gmail.com>
2022-01-23 17:26:11 +0100

    some fixes for stepper driver sketch

--- video/sketch/sketch.ino
+++ video/sketch/sketch.ino
@@ -3,32 +3,34 @@
 #define DIRECTION_PIN D7
 
 bool flag;
-int timeout;
 uint32_t timer;
 
 void setup()
 {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    pinMode(STEP_PIN, OUTPUT);
-    pinMode(DIRECTION_PIN, OUTPUT);
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(DIRECTION_PIN, OUTPUT);
 
-    digitalWrite(DIRECTION_PIN, HIGH);
+  digitalWrite(DIRECTION_PIN, HIGH);
 
-    flag = false;
-    timer = millis();
+  flag = false;
+  timer = micros();
 }
 
 void loop()
 {
-    digitalWrite(STEP_PIN, flag);
-    float val = analogRead(SPEED_PIN);
-    timeout = (int)((val / 1023.0) * 250.0);
+  digitalWrite(STEP_PIN, flag);
+  int val = analogRead(SPEED_PIN);
+  float timeout = (((float)val) / 1024.0) * 1000.0;
 
-    uint32_t now = millis();
-    uint32_t delay = now - timer;
-    if (delay > timeout)
-    {
-        flag = !flag;
-    }
+  Serial.println(timeout, 3);
+
+  uint32_t now = micros();
+  uint32_t delay = now - timer;
+  if (delay > timeout)
+  {
+    flag = !flag;
+    timer = now;
+  }
 }
