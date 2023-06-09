9881df2b36bc513c19ee979f51677d2fa7e472a0
dayaftereh <lasse.loeffler@gmail.com>
2023-06-10 00:44:15 +0200

    first run with launchpad

--- scripts/mock-launch-pad.ts
+++ scripts/mock-launch-pad.ts
@@ -1,9 +1,47 @@
 import { IncomingMessage } from 'http';
 import { WebSocket, WebSocketServer } from 'ws';
-import { WebMessage, WebMessageDecoder } from 'lrocket';
+import { WebMessageDecoder, WebMessage } from 'lrocket/services/web/messages/public-api'
+
+
+class LaunchPad {
+
+    private connectionId: number
+    private rockets: Map<number, WebSocket>
+    private controlCenters: Map<number, WebSocket>
+
+    private webSocketServer: WebSocketServer | undefined
+
+    constructor() {
+        this.connectionId = 0
+        this.rockets = new Map<number, WebSocket>()
+        this.controlCenters = new Map<number, WebSocket>()
+    }
+
+    init(): void {
+        this.webSocketServer = new WebSocketServer({ port: 8080, path: '/api/ws' })
+        this.webSocketServer.on("connection", (webSocket: WebSocket, request: IncomingMessage) => {
+
+        })
+    }
+
+    private onConnected(webSocket: WebSocket, request: IncomingMessage): void {
+        const id: number = this.connectionId++
+    }
+
+    private onClose(id: number, webSocket: WebSocket): void {
+
+    }
+
+    private onMessage(id: number, webSocket: WebSocket): void {
+
+    }
+
+}
 
 const clients: WebSocket[] = []
-const wss: WebSocketServer = new WebSocketServer({ port: 8080, path: '/api/ws' });
+const wss: WebSocketServer
+
+console.log(WebMessageDecoder.decode)
 
 wss.on('connection', function connection(webSocket: WebSocket, request: IncomingMessage) {
     clients.push(webSocket)
@@ -23,3 +61,5 @@ wss.on('connection', function connection(webSocket: WebSocket, request: Incoming
     })
 
 });
+
+console.log("started")
